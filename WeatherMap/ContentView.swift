d4f4add2fd1f95031198393e1bf0e40223a5c604
de4life <ageev_artur@ukr.net>
2022-10-27 00:57:12 +0300

    Initial Commit

---
+++ WeatherMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherMap
+//
+//  Created by Артур Агеев on 27.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherMap/WeatherMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherMapApp.swift
+//  WeatherMap
+//
+//  Created by Артур Агеев on 27.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
