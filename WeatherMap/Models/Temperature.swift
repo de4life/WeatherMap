dbcb30952b15abc43b7b8fe237098325d1a86fe1
de4life <ageev_artur@ukr.net>
2022-11-01 14:44:30 +0200

    add temp

---
+++ WeatherMap/Models/Temperature.swift
@@ -0,0 +1,14 @@
+//
+//  Temperature.swift
+//  WeatherMap
+//
+//  Created by Артур Агеев on 27.10.2022.
+//
+
+import Foundation
+
+
+struct Temperature: Codable {
+    var main: Double
+    var max: Double
+}
