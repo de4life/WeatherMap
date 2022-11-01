028a817cb5f015f70cfd2310b574ef9274159688
de4life <ageev_artur@ukr.net>
2022-11-01 14:44:10 +0200

    add daily weather

---
+++ WeatherMap/Models/DailyWeather.swift
@@ -0,0 +1,34 @@
+//
+//  DailyWeather.swift
+//  WeatherMap
+//
+//  Created by Артур Агеев on 27.10.2022.
+//
+
+import Foundation
+
+
+struct DailyWeather: Codable, Identifiable {
+    var dt: Int
+    var temp:Temperature
+    var weather: [WeatherDetail]
+    
+enum CodingKey: String {
+    case dt
+    case temp
+    case weather
+}
+    init() {
+        dt = 0
+        temp = Temperature(main: 0
+                           , max: 0.0)
+        weather = [WeatherDetail(main: "", description: "", icon: "")]
+    }
+}
+
+
+extension DailyWeather {
+    var id: UUID {
+        return UUID()
+    }
+}
