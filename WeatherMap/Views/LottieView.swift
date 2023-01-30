57a0298a134583c487d0d80fa0ad1e2fcc4dbbf6
de4life <ageev_artur@ukr.net>
2023-01-30 13:39:32 +0200

    add lotitieview

---
+++ WeatherMap/Views/LottieView.swift
@@ -0,0 +1,38 @@
+//
+//  LottieView.swift
+//  WeatherMap
+//
+//  Created by Артур Агеев on 28.01.2023.
+//
+
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    var name: String
+    var loopMode: LottieLoopMode = .loop
+    
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> some UIView {
+        let view = UIView()
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: UIViewRepresentableContext<LottieView>) {
+        uiView.subviews.forEach({$0.removeFromSuperview()})
+        
+        let animationView = LottieAnimationView()
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        uiView.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: uiView.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: uiView.heightAnchor)
+        ])
+        
+        animationView.animation = LottieAnimation.named(name)
+        animationView.contentMode = .scaleToFill
+        animationView.loopMode = loopMode
+        animationView.play()
+
+    }
+}
